71491a148f9c97d829627dfccaad4500aa4ce352
panllq <1115005803@qq.com>
2024-12-02 02:04:45 +0800

    传统rag

---
+++ agent_workflow/rag/__init__.py
@@ -0,0 +1,2 @@
+from .lightrag_mode import DocumentProcessor
+from .general_rag import GeneralRAG
--- agent_workflow/utils/__init__.py
+++ agent_workflow/utils/__init__.py
@@ -2,3 +2,5 @@ from .loading import LoadingIndicator, loadingInfo
 from .func import get_url, generate_random_filename, get_username_chatroom
 from .download import download_image, download_audio
 from .handler import VideoHandler, ImageHandler, VoiceHandler, FileHandler
+from .knowledge_graph_visualizer import KnowledgeGraphVisualizer
+from .read_files import ReadFiles
